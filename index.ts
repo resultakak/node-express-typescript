480ca79b1765858051f0f0db1db0bb7320653039
Resul Takak <resultakak@gmail.com>
2021-08-05 11:39:11 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log('Ready!');
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Ready!</h1>');
+});
+
+app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
